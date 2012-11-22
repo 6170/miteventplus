6b8ff4243630c73ff19f74c539e9b9251c3c91fe
adchia <dchiao@mit.edu>
2012-11-22 13:12:39 -0500

    added fullcalendar files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Risd::Application.routes.draw do
   root :to => "home#index"
   devise_for :users
-  resources :users, :only => [:index, :update, :destroy]
+  resources :users, :only => [:index, :update, :destroy, :show]
   resources :events
 end
