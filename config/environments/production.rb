9eff39995f647d8485cbc66c2f1eb60029305e2d
adchia <dchiao@mit.edu>
2012-11-25 15:10:49 -0500

    fixing production email validation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Risd::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'gmail.com' }
+  config.action_mailer.default_url_options = { :host => 'eventplus.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
