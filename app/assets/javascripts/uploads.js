36bb28ff181c15a4f84702c83caf69bbbbb383af
adchia <dchiao@mit.edu>
2012-12-04 02:28:53 -0500

    separated out into email page

---
+++ app/assets/javascripts/uploads.js
@@ -0,0 +1,47 @@
+/*# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/*/
+
+$(document).bind('dragover', function (e) {
+    var dropZone = $('#dropzone')
+    dropZone.addClass('hover');
+    if (e.target === dropZone[0]) {
+        dropZone.addClass('hover');
+    } else {
+        dropZone.removeClass('hover');
+    }
+});
+
+$(function () {
+      // Initialize the jQuery File Upload widget:
+      $('#fileupload').fileupload({
+        dropZone: $('#dropzone')
+      });
+      // 
+      // Load existing files:
+      $.getJSON($('#fileupload').prop('action'), function (files) {
+        var fu = $('#fileupload').data('fileupload'), 
+          template;
+        fu._adjustMaxNumberOfFiles(-files.length);
+        console.log(files);
+        template = fu._renderDownload(files)
+          .appendTo($('#fileupload .files'));
+        // Force reflow:
+        fu._reflow = fu._transition && template.length &&
+          template[0].offsetWidth;
+        template.addClass('in');
+        $('#loading').remove();
+      });
+
+  });
+  $(document).ready(function() {
+     var event_id = $('.redactor').attr('id');
+     $('.redactor').redactor({ 
+      imageUpload: "/events/" + event_id + "/uploadFromRedactor" + "?" + $('meta[name=csrf-param]').attr('content') + "=" + encodeURIComponent($('meta[name=csrf-token]').attr('content')),
+      imageGetJson: "/events/" + event_id + "/images.json",
+      imageUploadErrorCallback: callback
+     });
+  })
+  function callback(obj, json) {
+    alert(json.error);
+  }
