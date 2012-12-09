725c42e44230654f9cc491eb527a5a71c7b38e97
Sherwin Wu <thesherwinwu@gmail.com>
2012-12-09 15:09:31 -0500

    small changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -55,6 +55,7 @@ class EventsController < ApplicationController
       @event.checklist_items.create(:text => "Send posters to CopyTech to print and publicize your event.", :tag => "copytech")
       @event.checklist_items.create(:text => "Upload files to your event", :tag => "filemanager")
       @event.checklist_items.create(:text => "Send publicity emails", :tag => "publicity")
+      @event.checklist_items.create(:text => "Create a budget for your event", :tag => "budget")
       redirect_to :root
       return
     end
