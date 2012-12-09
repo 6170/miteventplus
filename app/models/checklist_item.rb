2b6d28bdaf8d2eefe978751a2840b0a61ee6f8d1
Sherwin Wu <thesherwinwu@gmail.com>
2012-12-09 16:17:45 -0500

    comments + added meeting agenda and notes

--- app/models/checklist_item.rb
+++ app/models/checklist_item.rb
@@ -4,11 +4,13 @@ class ChecklistItem < ActiveRecord::Base
 
   belongs_to :event
 
+  # sets the checked boolean of a checklist item to true
   def set_checked_true
     self.checked = true
     self.save
   end
 
+  # set the checked boolean of a checklist item to false
   def set_checked_false
     self.checked = false
     self.save
