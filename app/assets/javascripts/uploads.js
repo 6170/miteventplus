abfc36125c06b78d9e5acd49d732aaaaf95330cd
adchia <dchiao@mit.edu>
2012-12-09 16:08:49 -0500

    rerouting

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -2,6 +2,7 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/*/
 
+// Drop zone where user can drag and drop files
 $(document).bind('dragover', function (e) {
     var dropZone = $('#dropzone')
     dropZone.addClass('hover');
@@ -13,35 +14,38 @@ $(document).bind('dragover', function (e) {
 });
 
 $(function () {
-      // Initialize the jQuery File Upload widget:
-      $('#fileupload').fileupload({
-        dropZone: $('#dropzone')
-      });
-      // 
-      // Load existing files:
-      $.getJSON($('#fileupload').prop('action'), function (files) {
-        var fu = $('#fileupload').data('fileupload'), 
-          template;
-        fu._adjustMaxNumberOfFiles(-files.length);
-        console.log(files);
-        template = fu._renderDownload(files)
-          .appendTo($('#fileupload .files'));
-        // Force reflow:
-        fu._reflow = fu._transition && template.length &&
-          template[0].offsetWidth;
-        template.addClass('in');
-        $('#loading').remove();
-      });
-
+  // Initialize the jQuery File Upload widget:
+  $('#fileupload').fileupload({
+    dropZone: $('#dropzone')
   });
-  $(document).ready(function() {
-     var event_id = $(".publicity_email_form").attr('id');
-     $('.redactor').redactor({ 
-      imageUpload: "/events/" + event_id + "/uploadFromRedactor" + "?" + $('meta[name=csrf-param]').attr('content') + "=" + encodeURIComponent($('meta[name=csrf-token]').attr('content')),
-      imageGetJson: "/events/" + event_id + "/images.json",
-      imageUploadErrorCallback: callback
-     });
-  })
-  function callback(obj, json) {
-    alert(json.error);
-  }
+  // 
+  // Load existing files:
+  $.getJSON($('#fileupload').prop('action'), function (files) {
+    var fu = $('#fileupload').data('fileupload'), 
+      template;
+    fu._adjustMaxNumberOfFiles(-files.length);
+    console.log(files);
+    template = fu._renderDownload(files)
+      .appendTo($('#fileupload .files'));
+    // Force reflow:
+    fu._reflow = fu._transition && template.length &&
+      template[0].offsetWidth;
+    template.addClass('in');
+    $('#loading').remove();
+  });
+});
+
+// Initialize Redactor plugin
+$(function() {
+   var event_id = $(".publicity_email_form").attr('id');
+   $('.redactor').redactor({ 
+    imageUpload: "/events/" + event_id + "/uploadFromRedactor" + "?" + $('meta[name=csrf-param]').attr('content') + "=" + encodeURIComponent($('meta[name=csrf-token]').attr('content')),
+    imageGetJson: "/events/" + event_id + "/images.json",
+    imageUploadErrorCallback: callback
+   });
+});
+
+// Callback if file uploaded to redactor not an image
+function callback(obj, json) {
+  alert(json.error);
+}
