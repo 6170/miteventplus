527fdb99fe774cc8df84f53570ea794230ca8377
Sherwin Wu <thesherwinwu@gmail.com>
2012-12-10 08:52:00 -0500

    refactored code and js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,5 @@
 $(function(){
-  $(".foundicon-checkmark, .foundicon-remove").live('click', function() {
+  $(".checked, .unchecked").live('click', function() {
     if ($(this).attr('id') != "home-check") {
       $.ajax({
         type: "POST",
@@ -65,4 +65,53 @@ $(function(){
 
   if ($('.editableitem').length != 0)
     $(".editableitem").editable("/checklist_items/edit_text");
+
+  $(".new-tag").keypress(function(e) {
+    if (e.which == 13) {
+      var text = $(this).val();
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "/tags",
+        data: "name=" + text,
+        success: $.proxy(function(data) {
+          $(this).val('');
+          var div_start = '<div id="tag-' + data.id + '"" style="padding:5px;">';
+          var icon_code = '<i class="general foundicon-star"></i>';
+          var text_code = '<span class="bold">&nbsp;' + text + '</span>';
+          var delete_button = '<span class="right-align"><a href="#" class="delete-tag" id="' + data.id + '" data-confirm="Are you sure you want to delete this tag?">Delete</a></span>';
+          var ending = '<br></div>';
+
+          $(div_start + icon_code + text_code + delete_button + ending).hide().appendTo("#existing-tags").fadeIn(800);
+        }, this)
+      });
+    }
+  });
+
+  $(".delete-tag").live('click', function() {
+    var tag_id = $(this).attr('id');
+    $.ajax({
+      type: "DELETE",
+      url: "/tags/" + tag_id,
+      success: $.proxy(function() {
+        $("#tag-" + tag_id).fadeOut(300, function() {
+          $(this).remove();
+        })
+      }, this)
+    });
+    return false;
+  });
+
+  $("div[id|='tag']").live({
+    mouseenter:
+      function() {
+        $("a.delete-tag", this).show();
+        $(this).css("background", '#e0e0e0');
+      },
+    mouseleave:
+      function() {
+        $("a.delete-tag", this).hide();
+        $(this).css("background", 'transparent');
+      }
+  });
 });
