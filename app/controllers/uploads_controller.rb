44a6034fd5d763afeb75b2132f42fda13c62f542
adchia <dchiao@mit.edu>
2012-12-10 12:49:18 -0500

    upload fix

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -32,7 +32,7 @@
           :content_type => 'text/html',
           :layout => false
         }
-        format.json { render json: [@upload.to_jq_upload].to_json, status: :created, location: @upload }
+        format.json { render json: [@upload.to_jq_upload].to_json, status: :created, location: [@event, @upload] }
       else
         format.html { render action: "new" }
         format.json { render json: @upload.errors, status: :unprocessable_entity }
